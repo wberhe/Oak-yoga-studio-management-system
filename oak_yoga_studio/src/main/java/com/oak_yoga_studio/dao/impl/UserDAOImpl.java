3efdb684c02a9730611ebf7aee8e91676868c039
Fetiya <Fetiya@192.168.1.124>
2014-10-09 19:54:11 -0500

    userDAO added

---
+++ oak_yoga_studio/src/main/java/com/oak_yoga_studio/dao/impl/UserDAOImpl.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.oak_yoga_studio.dao.impl;
+
+/**
+ *
+ * @author Fetiya
+ */
+public class UserDAOImpl {
+    
+}
