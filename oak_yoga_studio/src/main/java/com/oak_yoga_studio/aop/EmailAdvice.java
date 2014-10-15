fe2d014c07aef9b32344e2694e409f08a3f56ae0
Weldu Berhe <wel.berhe@gmail.com>
2014-10-15 11:13:11 -0500

    aop is added

---
+++ oak_yoga_studio/src/main/java/com/oak_yoga_studio/aop/EmailAdvice.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.oak_yoga_studio.aop;
+
+
+
+import org.aspectj.lang.annotation.Aspect;
+
+/**
+ *
+ * @author Weldino
+ */
+@Aspect
+public class EmailAdvice {
+ 
+    
+}
