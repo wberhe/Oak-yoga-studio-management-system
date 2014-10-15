15313efe9476e67bd767a52320f661458e9545b0
Fetiya <Fetiya@10.10.14.121>
2014-10-14 19:12:21 -0500

    EnrollmentService implementation

--- oak_yoga_studio/src/main/java/com/oak_yoga_studio/service/IEnrollmentService.java
+++ oak_yoga_studio/src/main/java/com/oak_yoga_studio/service/IEnrollmentService.java
@@ -6,10 +6,51 @@
 
 package com.oak_yoga_studio.service;
 
+import com.oak_yoga_studio.domain.Course;
+import com.oak_yoga_studio.domain.Customer;
+import com.oak_yoga_studio.domain.Enrollment;
+import com.oak_yoga_studio.domain.Faculty;
+import com.oak_yoga_studio.domain.Section;
+import java.util.List;
+
 /**
  *
  * @author Weldu
  */
 public interface IEnrollmentService {
+    
+     public void addEnrollment(Enrollment enrollment);     
+  
+     public Enrollment getEnrollmentById(int id);
+     
+     public List<Enrollment> getAllEnrollments();
+     
+     public List<Enrollment> getEnrollmentsByCustomer(Customer cusomer);
+     
+     public List<Enrollment> getEnrollmentsByCustomerID(int customerID);
+      
+     public List<Section> getSections(Course course);
+
+     public boolean checkSeatAvailablity(int sectionID);
+     
+     public List<Course> getAllCourses();
+     
+     public void saveEnrollment(Enrollment enrollment);
+     
+     public void saveSection(Section section);
+
+     public Faculty getAvailableAdvisor();
+     
+     public void addWaitingListEnrollment(Enrollment enrollment);
+     
+     public List<Course> getWaivers(Customer customerID);
+     
+     public List<Course> getCoursesTaken(int customerID);
+     
+     public void withdraw(Customer customer, Section section);
+     
+     public Enrollment getTopWaitingList(int sectionId);
+     
+     public  void changeEnrollmentStatus(String status);
     
 }
